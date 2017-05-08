717d6c668889355ec6c05272627ab4252288b700
Jaime Vilca <jaime_v@outlook.com>
2017-05-07 20:03:31 -0500

    Implementando las operaciones para BuyerController

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Buyer;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Buyer;
 class BuyerController extends Controller
 {
     /**
@@ -14,28 +14,8 @@ class BuyerController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        $compradores = Buyer::has('transactions')->get();
+        return response()->json(['data' => $compradores], 200);
     }
 
     /**
@@ -46,40 +26,8 @@ class BuyerController extends Controller
      */
     public function show($id)
     {
-        //
+        $compradores = Buyer::has('transactions')->findOrFail($id);
+        return response()->json(['data' => $compradores], 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
